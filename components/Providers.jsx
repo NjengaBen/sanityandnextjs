c61186cbc6ae6b33e8f685f3bc149b7eef3aabb7
NjengaBen <bennjenga2@gmail.com>
2023-10-12 02:24:44 +0300

    theme:

--- components/Providers.jsx
+++ components/Providers.jsx
@@ -2,6 +2,6 @@
 import { ThemeProvider } from "next-themes";
 
 const Providers = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 };
 export default Providers;
