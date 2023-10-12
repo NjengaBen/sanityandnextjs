ab8912bc4dab37b85faeccab74a026bb48c7287d
NjengaBen <bennjenga2@gmail.com>
2023-10-12 20:16:48 +0300

    data

--- app/page.js
+++ app/page.js
@@ -1,3 +1,48 @@
-export default function Home() {
-  return <div>Welcome to Next.js Blog</div>;
+import Link from "next/link.js";
+import { client } from "./lib/sanity.jsx";
+async function getPosts() {
+  const query = '*[_type == "post"]';
+  const posts = await client.fetch(query);
+  return posts;
+}
+export default async function Home() {
+  const data = await getPosts();
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          All Posts
+        </h1>
+      </div>
+      <ul>
+        {data.map((post) => (
+          <li key={post._id} className="py-4">
+            <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+              <div>
+                <p className="text-base font-medium leading-6 text-teal-500">
+                  {/* {new Date(post._createdAt).toISOString().split("T")[0]} */}
+                  {new Date(post._createdAt).toLocaleDateString()}
+                </p>
+              </div>
+              <Link
+                href={`/post/${post.slug.current}`}
+                prefetch
+                className="space-y-2 xl:col-span-3"
+              >
+                <div>
+                  <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                    {post.title}
+                  </h3>
+                </div>
+
+                <p className="prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-2">
+                  {post.overview}
+                </p>
+              </Link>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
