756b1a81298e95d9eaa8c7ad14df550096c713cc
NjengaBen <bennjenga2@gmail.com>
2023-10-13 20:59:36 +0300

    text

---
+++ app/post/[slug]/page.jsx
@@ -0,0 +1,40 @@
+import { PortableText } from "@portabletext/react";
+import { client } from "@/app/lib/sanity";
+async function getData(slug) {
+  const query = `*[_type == "post" && slug.current == '${slug}'][0]`;
+  const data = await client.fetch(query);
+  return data;
+}
+
+const slugPage = async ({ params }) => {
+  const data = await getData(params.slug);
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <header className="pt-6 xl:pb-6">
+        <div className="space-y-1 text-center">
+          <div className="space-y-10">
+            <div>
+              <p className="text-base font-medium leading-6 text-teal-500">
+                {new Date(data._createdAt).toLocaleDateString()}
+              </p>
+            </div>
+          </div>
+          <div>
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-500 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+              {data.title}
+            </h1>
+          </div>
+        </div>
+      </header>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700 pb-7 xl:divide-y-0">
+        <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:cols-spa2-3 xl:row-span-2 xl:pb-0">
+          <div className="prose max-w-none pb-8 pt-10 dark:prose-invert prose-lg">
+            <PortableText value={data.content} components={{}} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default slugPage;
